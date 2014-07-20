e9818a9785745629b210202e1a80e1e72fb24546
Marcus <marcus.leung021@gmail.com>
2014-07-20 20:43:57 +0800

    First Commit

--- app/src/main/java/com/mstudio/myapplication/MyActivity.java
+++ app/src/main/java/com/mstudio/myapplication/MyActivity.java
@@ -2,18 +2,125 @@ package com.mstudio.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 
 public class MyActivity extends Activity {
+
+    private static final String TOTAL_BILL = "TOTAL_BILL";
+    private static final String CURRENT_TIP = "CURRENT_TIP";
+    private static final String BILL_WITHOUT_TIP = "BILL_WITHOUT_TIP";
+
+    private double billBeforeTip;
+    private double tipAmount;
+    private double finalBill;
+
+    EditText billBeforeTipET;
+    EditText tipAmountET;
+    EditText finalBillET;
+    SeekBar tipSeekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        if (savedInstanceState == null) {
+            // first time onCreate()
+
+            billBeforeTip = 0.0;
+            tipAmount = .15;
+            finalBill = 0.0;
+
+        } else {
+            billBeforeTip = savedInstanceState.getDouble(BILL_WITHOUT_TIP);
+            tipAmount = savedInstanceState.getDouble(CURRENT_TIP);
+            finalBill = savedInstanceState.getDouble(TOTAL_BILL);
+        }
+
+        billBeforeTipET = (EditText) findViewById(R.id.billEditText);
+        tipAmountET = (EditText) findViewById(R.id.tipEditText);
+        finalBillET = (EditText) findViewById(R.id.finalEditText);
+        tipSeekBar = (SeekBar) findViewById(R.id.ChangeTipSeekBar);
+
+        tipSeekBar.setOnSeekBarChangeListener(tipSeekBarListener);
+
+        billBeforeTipET.addTextChangedListener(billBeforeTipListener);
+
+
     }
 
+
+
+    private SeekBar.OnSeekBarChangeListener tipSeekBarListener = new SeekBar.OnSeekBarChangeListener(){
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            tipAmount = (tipSeekBar.getProgress()) * .01;
+            tipAmountET.setText(String.format("%.02f", tipAmount));
+
+
+            updateTipAndFinalBill();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+
+    private TextWatcher billBeforeTipListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                billBeforeTip = Double.parseDouble(s.toString());
+
+            } catch (NumberFormatException e) {
+                billBeforeTip = 0.0;
+            }
+
+            updateTipAndFinalBill();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private void updateTipAndFinalBill() {
+
+        double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
+        double finalBill = billBeforeTip + (billBeforeTip * tipAmount);
+
+        finalBillET.setText(String.format("%.02f", finalBill));
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+        outState.putDouble(TOTAL_BILL, finalBill);
+        outState.putDouble(CURRENT_TIP, tipAmount);
+        outState.putDouble(BILL_WITHOUT_TIP, billBeforeTip);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
